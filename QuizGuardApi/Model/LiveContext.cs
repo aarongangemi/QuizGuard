89e6306895817cc591010ff79baa09e690eac341
Aaron Gangemi <aaron.gangers@gmail.com>
2024-03-18 23:51:58 +0800

    Initial kickoff commit

---
+++ QuizGuardApi/Model/LiveContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizGuardApi.Model
+{
+    public class LiveContext : DbContext
+    {
+        public DbSet<QuizResult> QuizResults { get; set; }
+
+        public LiveContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
---
+++ QuizGuardApi/Model/QuizResult.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizGuardApi.Model
+{
+    public class QuizResult
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+    }
+}
